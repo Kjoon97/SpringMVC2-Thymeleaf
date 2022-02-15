6b42b8449a9df81dc3c09799e57e15bb2cede294
joonhyuck <jh83370@naver.com>
2022-02-15 16:10:11 +0900

    기본 객체들, 타임리프에서 편의 객체 제공(param, session, @)

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,13 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +28,7 @@ public class BasicController {
         return "basic/text-unescaped";
     }
 
+    //객체, 리스트, 맵 model에 넣기.
     @GetMapping("/variable")
     public String variable(Model model){
         User userA = new User("userA",10);
@@ -54,6 +57,21 @@ public class BasicController {
         public User(String username, int age) {
             this.username = username;
             this.age = age;
+        }
+    }
+
+    //세션 데이터
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");   //model에 담아도되고 이렇게 해도된다.
+        return "basic/basic-objects";
+    }
+
+    //빈 생성, 등록.
+    @Component("helloBean")
+    static class HelloBean{
+        public String hello(String data){
+            return "Hello"+data;
         }
     }
 }
